479244e3c8f72139c7150da31acbb5f6829b57e9
Elena <hardevich@gmail.com>
2023-04-21 21:36:35 +0400

    add hw02 task01

---
+++ lesson_02/hw02/task01/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+//Первый вариант решения
+
+System.Console.WriteLine("Введите трехзначное число: ");
+int num = 0;
+while (!int.TryParse(System.Console.ReadLine(), out num))
+{
+Console.WriteLine("This is not a number");
+}
+
+int secondNum = (num / 10) % 10;
+Console.WriteLine(secondNum);
