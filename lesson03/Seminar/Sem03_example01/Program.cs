ec2eac480f663b15f234f7f5035d341a803c9e2c
Elena <hardevich@gmail.com>
2023-04-24 23:53:37 +0400

    lesson 03 seminar03 example01

--- lesson03/Seminar/Sem03_example01/Program.cs
+++ lesson03/Seminar/Sem03_example01/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход
+// координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт
+// номер четверти плоскости, в которой находится эта
+// точка.
+
+int x = ReadInt("x");
+int y = ReadInt("y");
+
+if (isCoordinatesCorrect(x, y))
+  Console.WriteLine(GetQuarter(x, y));
+else
+  Console.WriteLine("Oooops");
+
+
+int ReadInt(string argument)
+{
+  Console.Write($"Input {argument}: ");
+
+  int i;
+
+  while (!int.TryParse(Console.ReadLine(), out i))
+  {
+    Console.WriteLine("it's not a number");
+  }
+
+  return i;
+}
+
+int GetQuarter(int x, int y)
+{
+  if (x > 0 && y > 0)
+  {
+    return 1;
+  }
+  else if (x < 0 && y > 0)
+  {
+    return 2;
+  }
+  else if (x < 0 && y < 0)
+  {
+    return 3;
+  }
+  else
+  {
+    return 4;
+  }
+}
+
+bool isCoordinatesCorrect(int x, int y)
+{
+  if (x == 0 || y == 0)
+  {
+    return false;
+  }
+  return true;
+}
