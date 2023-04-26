41e14830c10ed3847de931d42bdf83ef71b4d5b4
Elena <hardevich@gmail.com>
2023-04-26 22:58:16 +0400

    lesson 03 seminar03 example02-03

---
+++ lesson03/Seminar/Sem03_example02/Program.cs
@@ -0,0 +1,56 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает 
+// диапазон возможных координат точек в этой четверти (x и y)
+
+int quarter = ReadNum("number");
+// string coordinateX, coordinateY;
+
+if (isCoordinatesCorrect(quarter))
+  Console.WriteLine(GetCoordinate(coordinateX, coordinateY));
+else
+  Console.WriteLine("Oooops");
+
+int ReadNum(string argument)
+{
+  Console.Write($"Put a number from 1 to 4 {argument}: ");
+  int i;
+  while (!int.TryParse(Console.ReadLine(), out i))
+  {
+    Console.WriteLine("it's not a number");
+  }
+  return i;
+}
+
+int GetCoordinate(string coordinateX, string coordinateY)
+{
+  int num;
+
+  if (num == 1)
+  {
+    coordinateX = "from 0 to +infinity";
+    coordinateY = "from 0 to +infinity";
+  }
+  else if (num == 2)
+  {
+    coordinateX = "from -infinity to 0";
+    coordinateY = "from 0 to +infinity";
+  }
+    else if (num == 3)
+  {
+    coordinateX = "from -infinity to 0";
+    coordinateY = "from -infinity to 0";
+  }
+  else
+  {
+    coordinateX = "from 0 to +infinity";
+    coordinateY = "from -infinity to 0";
+  }
+}
+
+bool isCoordinatesCorrect(int i)
+{
+  if (i < 1 || i > 4)
+  {
+    return false;
+  }
+  return true;
+}
---
+++ lesson03/Seminar/Sem03_example03/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая
+// принимает на вход координаты двух точек и
+// находит расстояние между ними в 2D
+// пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+System.Console.WriteLine($"Put xA coordinate: ");
+double xA = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine($"Put xB coordinate: ");
+double xB = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine($"Put yA coordinate: ");
+double yA = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine($"Put yB coordinate: ");
+double yB = int.Parse(Console.ReadLine());
+
+double distance = Math.Sqrt((xB - xA) * (xB - xA) + (yB - yA) * (yB - yA));
+System.Console.WriteLine($"The distance beetween to points is {distance}");
