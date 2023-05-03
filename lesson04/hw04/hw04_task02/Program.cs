a0e5cd1ecce54aceabad68f5b322329c8b69a96f
Elena <hardevich@gmail.com>
2023-05-03 22:06:38 +0400

    add lesson04 hw01-02

---
+++ lesson04/hw04/hw04_task02/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+System.Console.WriteLine("Put a number: ");
+int firstNumb = int.Parse(Console.ReadLine());
+string firstNumbStr = firstNumb.ToString();
+
+for (int i = 0; i < firstNumbStr.Length; i++)
+{
+  int sum = firstNumbStr[i] + i;
+  // return sum;
+  System.Console.WriteLine(sum);
+}
