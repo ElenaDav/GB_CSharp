d8388b9eecc250a2b6edca6de81b58d8894e69a2
Elena <hardevich@gmail.com>
2023-05-03 23:25:34 +0400

    add lesson04 seminar04 example02

---
+++ lesson04/Seminar/Sem04_example02/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int ReadNum(string argument)
+{
+  Console.Write(argument);
+  int i;
+  while (!int.TryParse(Console.ReadLine(), out i))
+  {
+    Console.WriteLine("it's not a number");
+  }
+  return i;
+}
+
+int [] ArrayNum(int number)
+{
+  string str = number.ToString();
+  int[] S = new int [str.Length];
+
+  for (int i = 0; i < str.Length; i++)
+  {
+    S[i] = int.Parse(str[i].ToString());
+  }
+  return S;
+}
+
+int num = ReadNum("Put a number: ");
+System.Console.WriteLine(ArrayNum(num).Length);
