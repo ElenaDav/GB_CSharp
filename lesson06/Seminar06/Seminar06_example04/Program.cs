34c4906fcd853b09c62b8c4364b29c46d46f3582
Elena <hardevich@gmail.com>
2023-05-13 18:55:00 +0400

    add lesson06 seminar06 example 01-05

---
+++ lesson06/Seminar06/Seminar06_example04/Program.cs
@@ -0,0 +1,44 @@
+﻿/* Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи.
+Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8*/
+
+int N = ReadInt("Put a number: ");
+GetNumbersFib(N);
+
+int ReadInt(string argument) // проверка на число
+{
+  Console.Write(argument);
+  int i;
+  while (!int.TryParse(Console.ReadLine(), out i))
+  {
+  System.Console.WriteLine("It is not a number!");
+  Console.Write(argument);
+  }
+  return i;
+}
+
+void GetNumbersFib(int n)
+{
+  int[] array = new int[n];
+  array[0] = 0;
+  array[1] = 1;
+
+  for (int i = 2; i < array.Length; i++)
+  {
+    array[i] = array[i - 1] + array[i - 2];
+  }
+  System.Console.WriteLine(string.Join(" , ", array));
+}
+
+// Рекурсия с фибоначчи
+// int Fibonachi(int n)
+// {
+//     if (n == 0 || n == 1) return n;
+//     return Fibonachi(n - 1) + Fibonachi(n - 2);
+// }
+
+// int fib45 = Fibonachi(45);
+// System.Console.WriteLine(fib45);
+
---
+++ lesson06/Seminar06/Seminar06_example05/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного
+// массива с помощью поэлементного копирования.
+
+void FillArray(int[] numbers, int minValue, int maxValue)
+{
+  maxValue++;
+  Random rnd = new Random();
+  for (int i = 0; i < numbers.Length; i++)
+  {
+    numbers[i] = rnd.Next(minValue, maxValue);
+  }
+}
+
+void PrintArray(int[] numbers)
+{
+  for (int i = 0; i < numbers.Length; i++)
+  {
+    Console.Write($"{numbers[i]} ");
+  }
+  Console.WriteLine();
+}
+
+int[] GetCopy(int[] array)
+{
+  int[] copyArray = new int[array.Length];
+  for (int i = 0; i < array.Length; i++)
+  {
+    copyArray[i] = array[i];
+  }
+  return copyArray;
+}
+
+void GetArray()
+{
+  System.Console.WriteLine("How long is the array?");
+  int n = int.Parse(Console.ReadLine());
+  int[] ourArray = new int[n];
+  FillArray(ourArray, -30, 99);
+  PrintArray(ourArray);
+  PrintArray(GetCopy(ourArray));
+}
+
+GetArray();
