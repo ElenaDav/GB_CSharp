149b54b6fa708a361a20d068c5c87d41c8fcb293
Elena <hardevich@gmail.com>
2023-05-14 22:31:49 +0400

    add hw06

--- lesson06/hw06/hw06_task02/Program.cs
+++ lesson06/hw06/hw06_task02/Program.cs
@@ -2,3 +2,23 @@
 заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
 задаются пользователем.    b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
 
+double k1 = ReadInt("Put k1: ");
+double b1 = ReadInt("Put b1: ");
+double k2 = ReadInt("Put k2: ");
+double b2 = ReadInt("Put b2: ");
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+double ReadInt(string argument)
+{
+  Console.Write($"Input {argument}: ");
+  double i;
+  while (!double.TryParse(Console.ReadLine(), out i))
+  {
+    Console.WriteLine("it's not a number");
+  }
+  return i;
+}
+
+Console.WriteLine($"Point of intersection of two lines is X:{x}  Y:{y}");
