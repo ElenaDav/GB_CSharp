60ab0d7af7c11ed21371356146dc0d9ceeaedf31
Elena <hardevich@gmail.com>
2023-05-29 22:35:59 +0400

    add seminar08 task 53, 55

--- lesson08/seminar08/Program.cs
+++ lesson08/seminar08/Program.cs
@@ -1,2 +1,114 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int ReadInt(string argument) // проверка на число
+{
+  int i;
+  while (!int.TryParse(Console.ReadLine(), out i))
+  {
+  System.Console.WriteLine("It is not a number!");
+  }
+  return i;
+}
+
+void CreateTwoArray(int[,] array)
+{
+  Random rnd = new Random();
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = rnd.Next(1,10);
+    }
+  }
+}
+
+void PrintTwoArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      System.Console.Write($"{array[i, j]} ");
+    }
+    System.Console.WriteLine();
+  }
+}
+
+// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет 
+// местами первую и последнюю строку массива.
+
+// Task53();
+
+void ChangeString(int[,] array)
+{
+  int tempArray = array[0, 0];
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      tempArray = array[0, j];
+      array[0, j] = array[array.GetLength(0) - 1, j];
+      array[array.GetLength(0) - 1, j] = tempArray;
+    }
+  }
+}
+
+void Task53()
+{
+  System.Console.WriteLine("Put M: ");
+  int m2 = ReadInt("M");
+  System.Console.WriteLine("Put N: ");
+  int n2 = ReadInt("N");
+  int[,] array = new int[m2, n2];
+
+  System.Console.WriteLine();
+  CreateTwoArray(array);
+  PrintTwoArray(array);
+  System.Console.WriteLine();
+  ChangeString(array);
+  PrintTwoArray(array);
+}
+
+
+// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки 
+// на столбцы. В случае, если это невозможно, программа должна вывести сообщение для
+// пользователя.
+
+Task55();
+int[,] ChangeStringsToColumns(int[,] array)
+{
+  int[,] tempArray = new int[array.GetLength(1), array.GetLength(0)];
+  if (array.GetLength(0) != array.GetLength(1))
+    {
+      System.Console.WriteLine("Strings not equal columns!");
+    }
+  else
+  {
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+      for (int j = 0; j < array.GetLength(1); j++)
+      {
+      tempArray[j, i] = array[i, j];
+      }
+    }
+  }
+    PrintTwoArray(tempArray);
+    return tempArray;
+}
+
+void Task55()
+{
+  System.Console.WriteLine("Put M: ");
+  int m2 = ReadInt("M");
+  System.Console.WriteLine("Put N: ");
+  int n2 = ReadInt("N");
+  int[,] array = new int[m2, n2];
+
+  System.Console.WriteLine();
+  CreateTwoArray(array);
+  PrintTwoArray(array);
+  System.Console.WriteLine();
+  ChangeStringsToColumns(array);
+}
+
+/* Задача 57: Составить частотный словарь элементов двумерного массива. Частотный 
+словарь содержит информацию о том, сколько раз встречается элемент входных данных.*/
+
